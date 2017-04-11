7b8ebe13fb8b62e2c54d6397e79729f99699fe05
francois guerin <francois.theo.guerin@outlook.com>
2017-04-11 17:05:24 -0300

    done

--- rails-wikinimous/db/seeds.rb
+++ rails-wikinimous/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+(1..50).to_a.each do
+  Article.create(title: Faker::Book.title, content: Faker::Lorem.paragraph)
+end
